3412795afbb900dbe49d6ced006c2c52c6894800
jeffbartow <jeff.bartow@gmail.com>
2019-02-23 14:33:50 +0000

    with javascript added

--- script.js
+++ script.js
@@ -32,7 +32,17 @@ var s= add(1,2);
 alert(s);
 */
 
+/*
 var myName="Jeff Bartow";
 var names=myName.split(" ");
 alert(names);
 console.log(names);
+
+console.log(names[0]);
+console.log(names.join("-"));
+*/
+
+var paragraph=document.createElement('p');
+paragraph.innerText= "Hi from Jeff";
+//<p> hi from Jeff </p>
+document.querySelector("body").appendChild(paragraph);
