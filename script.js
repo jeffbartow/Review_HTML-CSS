0d1bd51dd64f70ab95ebbd4764b939926e630d59
jeffbartow <jeff.bartow@gmail.com>
2019-02-23 13:58:47 +0000

    javascript exercise add-on

---
+++ script.js
@@ -0,0 +1,38 @@
+/*
+var n=1;
+alert(n);
+var n=1+2;
+alert(n);
+var n=1+5/2;
+alert(n);
+var z=2*n;
+alert(z);
+*/
+
+/*
+var s="hi "
+alert(s);
+var t="Jeff";
+var w=s+t;
+alert(w);
+*/
+
+/*
+var a=5+t;
+alert(a);
+var b=a*3;
+alert(b);
+*/
+
+/*
+function add(n1,n2){
+    return n1+n2;
+}
+var s= add(1,2);
+alert(s);
+*/
+
+var myName="Jeff Bartow";
+var names=myName.split(" ");
+alert(names);
+console.log(names);
